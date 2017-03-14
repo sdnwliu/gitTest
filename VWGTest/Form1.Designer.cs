844cc5e97d1fcaebf9e6bc4abbab6e009eb5f37f
sdnwliu <sdnwliu@126.com>
2017-03-14 22:36:54 +0800

    添加项目文件。

---
+++ VWGTest/Form1.Designer.cs
@@ -0,0 +1,66 @@
+namespace VWGTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Visual WebGui Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new Gizmox.WebGUI.Forms.Button();
+            this.button2 = new Gizmox.WebGUI.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            // 
+            // Form1
+            // 
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Size = new System.Drawing.Size(508, 372);
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gizmox.WebGUI.Forms.Button button1;
+        private Gizmox.WebGUI.Forms.Button button2;
+    }
+}
---
+++ VWGTest/Form1.cs
@@ -0,0 +1,25 @@
+#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+
+using Gizmox.WebGUI.Common;
+using Gizmox.WebGUI.Forms;
+
+#endregion
+
+namespace VWGTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
